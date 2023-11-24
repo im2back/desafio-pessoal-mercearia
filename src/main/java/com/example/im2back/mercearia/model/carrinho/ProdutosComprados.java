67a98aab00d8f5e04c6e7863c8135ae176e86b54
Jefferson Richards <jefferson.richards@outlook.com>
2023-11-23 22:08:16 -0300

    Implementados testes dos repositorios

--- src/main/java/com/example/im2back/mercearia/model/carrinho/ProdutosComprados.java
+++ src/main/java/com/example/im2back/mercearia/model/carrinho/ProdutosComprados.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity(name = "ProdutosComprados")
 @Table(name = "produtosComprados")
+@AllArgsConstructor
 public class ProdutosComprados {
 
 	
